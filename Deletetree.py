77074b0b1a0cb7c379a9f9afed633cb35cf3c05b
shreyaasridhar <shreyanju@gmail.com>
2016-04-04 22:19:29 +0530

    added

---
+++ Deletetree.py
@@ -0,0 +1,23 @@
+from binarysearchtree import BinarySearchTree
+
+def testtree():
+    T=BinarySearchTree()
+    n=int(raw_input())
+    for i in range(n):
+        T.insertElement(int(raw_input("Enter element "+str(i+1)+" : ")))
+    x=int(raw_input("Enter the element you want to delete: "))
+    T.deleteElement(x)
+    print "The tree is:" 
+    T.inorderTraverse(T.root)
+
+
+def main():
+    testtree()
+        
+    
+if __name__ == '__main__':
+    main()
+        
+        
+    
+    
